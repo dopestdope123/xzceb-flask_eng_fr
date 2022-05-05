0c9d5d9dab41ae3b9dade612995ee0a3ac1378c3
dopestdope123 <ptqmtl117@gmail.com>
2022-05-05 00:25:05 +0000

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,31 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+
+authenticator = IAMAuthenticator('{apikey}')
+language_translator = LanguageTranslatorV3(
+    version='{version}',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
+
+language_translator.set_disable_ssl_verification(True)
+
+
+def englishToFrench(englishText):
+    
+    frenchText=language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result() #get the translation 
+    frenchText=frenchText["translations"][0]['translation'] #Extract the text from the dictionary
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    englishText=language_translator.translate(
+    text=frenchText,
+    model_id='fr-en').get_result()
+    englishText=englishText["translations"][0]['translation'] #Extract the text from the dictionary
+    return englishText
